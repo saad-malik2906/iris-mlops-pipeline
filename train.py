f7ecb88a9ba6cfe337ce984bbf52e99fab464e18
Saad Malik <saadmalik29062002@gmail.com>
2025-07-21 20:07:16 +0500

    Add training and CI/CD pipeline

---
+++ train.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+import joblib
+import mlflow
+import os
+
+mlflow.set_tracking_uri("file:///C:/Users/DELL/Desktop/Projects/Everything/Project 07- MLOps Model Training and Deployment/mlruns")
+mlflow.set_experiment("iris-experiment")
+
+def train():
+    iris = load_iris()
+    X = pd.DataFrame(iris.data, columns=iris.feature_names)
+    y = pd.Series(iris.target)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    
+    with mlflow.start_run():
+        model.fit(X_train, y_train)
+        preds = model.predict(X_test)
+        acc = accuracy_score(y_test, preds)
+
+        mlflow.log_param("n_estimators", 100)
+        mlflow.log_metric("accuracy", acc)
+
+        joblib.dump(model, "best_model.pkl")
+        mlflow.log_artifact("best_model.pkl")
+
+        cm = confusion_matrix(y_test, preds)
+        plt.figure(figsize=(6, 4))
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
+        plt.title("Confusion Matrix")
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        plt.savefig("confusion_matrix.png")
+        mlflow.log_artifact("confusion_matrix.png")
+
+if __name__ == "__main__":
+    train()
